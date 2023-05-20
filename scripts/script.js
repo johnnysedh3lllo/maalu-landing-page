0b25726596d53942354900a0c6d25a9daa238d30
Johnny <104537366+johnnysedh3lllo@users.noreply.github.com>
2023-05-20 15:06:22 +0100

    worked on responsive mobile view, slider component for the header section

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,82 @@
 "use strict";
 
+const allSlides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+const dotsContainer = document.querySelector(".dots-container");
+const dots = document.querySelectorAll(".dots");
+
 const questionWrapper = document.querySelectorAll(".question-wrapper");
 const question = document.querySelectorAll(".question");
 const answer = document.querySelectorAll(".answer");
+const faqSection = document.querySelector(".faq-content");
 
-for (const item of questionWrapper) {
-  item.addEventListener("click", () => {
-    // console.log(item);
-    // console.log(index, item);
-    item.classList.toggle("active--section");
-    item.classList.toggle("active");
+// Slider Component
+
+// if (!s.classList.contains("one")) {
+//   s.style.display = "none";
+// }
+
+let currentSlide = 0;
+let numberOfSlides = allSlides.length;
+const createDots = () => {
+  allSlides.forEach((_, i) => {
+    dotsContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots" data-slide="${i}"></button>`
+    );
   });
-}
+};
 
-// question.forEach((faq) => {
-//   faq.addEventListener("click", () => {
-//     faq.classList.toggle("rotated");
-//   });
-// });
+const activateDots = (slide) => {
+  document
+    .querySelectorAll(".dots")
+    .forEach((dot) => dot.classList.remove("dots--active"));
+
+  document
+    .querySelector(`.dots[data-slide="${slide}"]`)
+    .classList.add("dots--active");
+};
+
+const goToSlide = (slide) => {
+  allSlides.forEach((s, i) => {
+    // if (!s.classList.contains("one")) {
+    //   s.classList.add("nodisplay")
+    // }
+    s.style.transform = `translateX(${100 * (i - slide)}%)`;
+  });
+};
+
+const nextSlide = () => {
+  if (currentSlide === numberOfSlides - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  goToSlide(currentSlide);
+  activateDots(currentSlide);
+};
+
+createDots();
+
+window.addEventListener("load", () => {
+  const viewportWidth = window.innerWidth;
+  const intervalCallback = () => nextSlide();
+  if (viewportWidth < 768) {
+    goToSlide(0);
+    activateDots(0);
+    setInterval(intervalCallback, 3000);
+  } else {
+    dotsContainer.remove()
+  }
+});
+
+//event delegation, for opening and closing of FAQ section
+faqSection.addEventListener("click", (e) => {
+  const clicked = e.target;
+  const parentOfClicked = clicked.closest(".question-wrapper");
+  if (parentOfClicked) {
+    parentOfClicked.classList.toggle("active--section");
+    parentOfClicked.classList.toggle("active");
+  }
+});
