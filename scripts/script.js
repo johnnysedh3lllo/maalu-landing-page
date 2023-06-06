84f2a85bf9423d460c4fac5a157fb005420887ee
Johnny <104537366+johnnysedh3lllo@users.noreply.github.com>
2023-06-06 14:55:39 +0100

    implemented full nav menu animation, reveal on scroll, mobile view, card slider etc

--- scripts/script.js
+++ scripts/script.js
@@ -1,76 +1,76 @@
 "use strict";
 
-const allSlides = document.querySelectorAll(".slide");
-const btnLeft = document.querySelector(".slider__btn--left");
-const btnRight = document.querySelector(".slider__btn--right");
-const dotsContainer = document.querySelector(".dots-container");
-const dots = document.querySelectorAll(".dots");
+//GENERAL SELECTIONS
+const sections = document.querySelectorAll(".observe-section");
+const navLinkContainer = document.querySelector(".navbar__list");
 
+//TEAM CAROUSEL
+const wrapper = document.querySelector(".team__grid-wrapper");
+const teamGrid = document.querySelector(".team__grid");
+const oneCardWidth = document.querySelector(".member-card").offsetWidth;
+const cards = [...document.querySelectorAll(".member-card")];
+
+//FAQ SECTION
 const questionWrapper = document.querySelectorAll(".question-wrapper");
 const question = document.querySelectorAll(".question");
 const answer = document.querySelectorAll(".answer");
 const faqSection = document.querySelector(".faq-content");
 
-// Slider Component
+///////////////////
+//*PAGE NAVIGATION
+navLinkContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  const clicked = e.target;
+  if (clicked.classList.contains("navbar__link")) {
+    const id = clicked.getAttribute("href");
 
-// if (!s.classList.contains("one")) {
-//   s.style.display = "none";
-// }
-
-let currentSlide = 0;
-let numberOfSlides = allSlides.length;
-const createDots = () => {
-  allSlides.forEach((_, i) => {
-    dotsContainer.insertAdjacentHTML(
-      "beforeend",
-      `<button class="dots" data-slide="${i}"></button>`
-    );
-  });
-};
-
-const activateDots = (slide) => {
-  document
-    .querySelectorAll(".dots")
-    .forEach((dot) => dot.classList.remove("dots--active"));
-
-  document
-    .querySelector(`.dots[data-slide="${slide}"]`)
-    .classList.add("dots--active");
-};
-
-const goToSlide = (slide) => {
-  allSlides.forEach((s, i) => {
-    // if (!s.classList.contains("one")) {
-    //   s.classList.add("nodisplay")
-    // }
-    s.style.transform = `translateX(${100 * (i - slide)}%)`;
-  });
-};
-
-const nextSlide = () => {
-  if (currentSlide === numberOfSlides - 1) {
-    currentSlide = 0;
-  } else {
-    currentSlide++;
-  }
-  goToSlide(currentSlide);
-  activateDots(currentSlide);
-};
-
-createDots();
-
-window.addEventListener("load", () => {
-  const viewportWidth = window.innerWidth;
-  const intervalCallback = () => nextSlide();
-  if (viewportWidth < 768) {
-    goToSlide(0);
-    activateDots(0);
-    setInterval(intervalCallback, 3000);
-  } else {
-    dotsContainer.remove()
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
   }
 });
 
+///////////////////
+//*NAV MENU DISPLAY
+const navMenuButton = document.querySelector(".nav-menu-button");
+const navMenu = document.querySelector(".nav-menu");
+
+navMenuButton.addEventListener("click", function (e) {
+  const clicked = e.target;
+  if (clicked.classList.contains("nav-menu-button")) {
+    navMenu.classList.add("nav-menu-display");
+  }
+});
+
+navMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const clicked = e.target.closest(".nav-menu__item");
+  if (clicked) {
+    const id = clicked.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+
+    setTimeout(() => {
+      navMenu.classList.remove("nav-menu-display");
+    }, 1000);
+  }
+});
+
+/////////////////
+//*TEAM CAROUSEL
+wrapper.addEventListener("click", (e) => {
+  const clicked = e.target;
+  if (clicked.classList.contains("btn-container")) return;
+  if (clicked.id === "right") {
+    teamGrid.scrollLeft += oneCardWidth;
+  } else {
+    teamGrid.scrollLeft += -oneCardWidth;
+  }
+});
+
+//*FAQ SECTION
 //event delegation, for opening and closing of FAQ section
 faqSection.addEventListener("click", (e) => {
   const clicked = e.target;
@@ -79,4 +79,24 @@ faqSection.addEventListener("click", (e) => {
     parentOfClicked.classList.toggle("active--section");
     parentOfClicked.classList.toggle("active");
   }
+});
+
+//* REVEAL SECTIONS ON SCROLL
+const callback = (entries) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return; //Guard Clause
+  if (entry.isIntersecting) entry.target.classList.add("display");
+
+  sectionObserver.unobserve(entry.target);
+};
+
+const configObject = {
+  root: null,
+  threshold: 0.1,
+};
+
+const sectionObserver = new IntersectionObserver(callback, configObject);
+
+sections.forEach((section) => {
+  sectionObserver.observe(section);
 });
